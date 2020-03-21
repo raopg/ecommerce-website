fa66758204b285876fbb3630f91e2e064437cfe9
raopg <prateek.rao.08@gmail.com>
2020-03-20 21:07:10 -0700

    Implemented Sign-in, form input and custom button components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header-component';
-
-const HatsPage = () => (
-  <div>
-    HATS PAGE!
-  </div>
-)
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -19,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
