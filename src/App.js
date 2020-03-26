f46ea36e49190dd92fbd37bf71665bdb0a841598
raopg <prateek.rao.08@gmail.com>
2020-03-25 22:01:35 -0700

    Implemented redux; added logger middleware, and set up user login/logout actions and reducers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Switch, Route} from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
@@ -20,23 +24,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapshot.id,
               ...snapshot.data()
-            }
           });
 
          // console.log(this.state);
         })
       }
       else {
-        this.setState({currentUser: null});
+        setCurrentUser(userAuth);
       }
       
     })
@@ -49,7 +53,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -60,4 +64,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null,mapDispatchToProps )(App);
