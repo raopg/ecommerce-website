5e91a4b5fe7dea9a77b4df82c258805e20c38885
raopg <prateek.rao.08@gmail.com>
2020-03-30 16:01:22 -0700

    Implemented selectors to optimize state changes

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
+
+// Selectors -> take a part of the state, do some computation and use that information.
+// This is inefficient code, because it triggers a re-render EVERY time the state changes.
+// Solution - cache the value [memoization]
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+});
 
 const mapDispatchToProps = dispatch  => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 export default connect(
-    null, 
+    mapStateToProps, 
     mapDispatchToProps
     )(CartIcon);
