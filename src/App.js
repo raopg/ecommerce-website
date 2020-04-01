5ecd243ad42e2d3a9def7cc13e7916f6f8d0e5e4
raopg <prateek.rao.08@gmail.com>
2020-04-01 15:41:29 -0700

    Added checkout page. Updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header-component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'; 
+import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component {
   constructor(props){
@@ -60,6 +61,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout'component={CheckoutPage} />
           <Route exact 
           path='/signin' 
           render={() => 
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -22,3 +22,14 @@ export const selectCartItemsCount = createSelector(
         )
 );
 
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumulatedQuantity, cartItem) =>
+                accumulatedQuantity + cartItem.quantity * cartItem.price,
+            0
+        )
+
+)
+
