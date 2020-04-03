65c9752acd432a0519f66273d928e7bdc478669a
raopg <prateek.rao.08@gmail.com>
2020-04-02 18:37:45 -0700

    Updated app with persistent local storage with redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middleWares = [logger];
 
-export default createStore(rootReducer, applyMiddleware(...middleWares));
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+export const persistor = persistStore(store);
+
+export default {
+    store,
+    persistor
+}
 
